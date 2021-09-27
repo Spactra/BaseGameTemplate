59f1adb4ae74ef996e6b11a79a534641bcbc68f3
mucoimaginite <89004580+mucoimaginite@users.noreply.github.com>
2021-09-27 11:23:21 +0300

    First Set up

---
+++ Assets/Assets/Scripts/Managers/VibrationManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using MoreMountains.NiceVibrations;
+public class VibrationManager : MonoBehaviour
+{
+    #region Singleton
+    public static VibrationManager Instance = null;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        Instance = this;
+    }
+    #endregion
+
+    public void SuccesVibration()
+    {
+        MMVibrationManager.Haptic(HapticTypes.Success, false, true, this);
+    }
+    public void HeavyVibration()
+    {
+        MMVibrationManager.Haptic(HapticTypes.HeavyImpact, false, true, this);
+    }
+    public void MediumVibration()
+    {
+        MMVibrationManager.Haptic(HapticTypes.MediumImpact, false, true, this);
+    }
+    public void LightVibration()
+    {
+        MMVibrationManager.Haptic(HapticTypes.LightImpact, false, true, this);
+    }
+}
